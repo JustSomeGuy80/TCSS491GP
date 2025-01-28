efa3db57fcb4fd7257f3137700bb412dec8d805e
Tyler Cairney <tylercairney99@gmail.com>
2025-01-27 19:04:50 -0800

    tetststs

--- src/engine/gameengine.js
+++ src/engine/gameengine.js
@@ -1,5 +1,4 @@
 // This game shell was happily modified from Googler Seth Ladd's "Bad Aliens" game and his Google IO talk in 2011
-/** @typedef {import("./timer")} */
 
 class GameEngine {
     constructor(options) {
@@ -11,40 +10,38 @@ class GameEngine {
         this.entities = [];
 
         // Information on the input
-        this.click = [null];
+        this.click = null;
         this.mouse = null;
         this.wheel = null;
         this.keys = {};
-        this.buttons = {};
 
         // Options and the Details
         this.options = options || {
             debugging: false,
         };
-    }
+    };
 
     init(ctx) {
         this.ctx = ctx;
         this.startInput();
         this.timer = new Timer();
-    }
+    };
 
     start() {
         this.running = true;
         const gameLoop = () => {
             this.loop();
-            // revert to old requestAnimFrame if anything goes wrong
-            window.requestAnimationFrame(gameLoop, this.ctx.canvas);
+            requestAnimFrame(gameLoop, this.ctx.canvas);
         };
         gameLoop();
-    }
+    };
 
     startInput() {
         const getXandY = e => ({
             x: e.clientX - this.ctx.canvas.getBoundingClientRect().left,
-            y: e.clientY - this.ctx.canvas.getBoundingClientRect().top,
+            y: e.clientY - this.ctx.canvas.getBoundingClientRect().top
         });
-
+        
         this.ctx.canvas.addEventListener("mousemove", e => {
             if (this.options.debugging) {
                 console.log("MOUSE_MOVE", getXandY(e));
@@ -75,16 +72,18 @@ class GameEngine {
             this.rightclick = getXandY(e);
         });
 
-        this.ctx.canvas.addEventListener("keydown", event => (this.keys[event.key] = true));
-        this.ctx.canvas.addEventListener("keyup", event => (this.keys[event.key] = false));
-
-        this.ctx.canvas.addEventListener("mousedown", event => (this.buttons[event.button] = true));
-        this.ctx.canvas.addEventListener("mouseup", event => (this.buttons[event.button] = false));
+        // Move keyboard listeners to window instead of canvas
+        window.addEventListener("keydown", event => {
+            this.keys[event.key] = true;
+        });
+        window.addEventListener("keyup", event => {
+            this.keys[event.key] = false;
+        });
     }
 
     addEntity(entity) {
         this.entities.push(entity);
-    }
+    };
 
     draw() {
         // Clear the whole canvas with transparent color (rgba(0, 0, 0, 0))
@@ -94,7 +93,7 @@ class GameEngine {
         for (let i = this.entities.length - 1; i >= 0; i--) {
             this.entities[i].draw(this.ctx, this);
         }
-    }
+    };
 
     update() {
         let entitiesCount = this.entities.length;
@@ -112,13 +111,14 @@ class GameEngine {
                 this.entities.splice(i, 1);
             }
         }
-    }
+    };
 
     loop() {
         this.clockTick = this.timer.tick();
         this.update();
         this.draw();
-    }
-}
+    };
+
+};
 
 // KV Le was here :)

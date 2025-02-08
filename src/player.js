7038d9fd80d17854e3e0f9ddb5b44772b7a68bc4
Hai Duong <149478749+HaixD@users.noreply.github.com>
2025-02-07 22:36:25 -0800

    update ColliderRect and player collision

--- src/player.js
+++ src/player.js
@@ -27,8 +27,24 @@ class Player {
         const width = 56;
 
         this.position = new Position(525, 500);
-        this.collider = new ColliderRect(this.position, -width/2, -height/2, width, height, 0, this);
-        this.teleport = new Teleport(game, assetManager, this, -width/2, -height/2, width, height)
+        this.collider = new ColliderRect(
+            this.position,
+            -width / 2,
+            -height / 2,
+            width,
+            height,
+            0,
+            this
+        );
+        this.teleport = new Teleport(
+            game,
+            assetManager,
+            this,
+            -width / 2,
+            -height / 2,
+            width,
+            height
+        );
         this.arm = new Arm(game, this.assetManager, this, 6, -4, "bladed");
         this.sprite = new Sprite(this.position, this.game, 3, -48, -48, {
             idle: new Animator(this.assetManager.getAsset("anims/idle.png"), 0, 0, 32, 32, 2, 2),
@@ -135,11 +151,10 @@ class Player {
         if (this.game.keys["a"]) move -= 1;
 
         if (this.game.keys[" "]) {
-            if (grounded && (this.jumped == 0 || this.jumpBuffer <= .1)) {
+            if (grounded && (this.jumped == 0 || this.jumpBuffer <= 0.1)) {
                 this.velocity.y = -this.jumpHeight;
                 this.jumped = 1;
                 this.assetManager.playAsset("sounds/jump.mp3");
-
             }
             this.jumpBuffer += this.game.clockTick;
         } else {
@@ -191,7 +206,8 @@ class Player {
             (move == 0 ||
                 (move == 1 && this.velocity.x < 0) ||
                 (move == -1 && this.velocity.x > 0) ||
-                (this.velocity.x > this.maxSpeed || this.velocity.x < -this.maxSpeed));
+                this.velocity.x > this.maxSpeed ||
+                this.velocity.x < -this.maxSpeed);
         if (traction) {
             // Apply ground friction
             if (this.velocity.x < 0) this.velocity.x += this.walkAccel * this.game.clockTick;
@@ -213,76 +229,25 @@ class Player {
     }
 
     runCollisions(origin) {
-        // TEMPORARY IMPLEMENTATION OF HITBOXES
-        // bugs:
-        // - (sort of a bug) when you are in the left side of a wall and go left, you tp to the right of wall
-        //      - to test, spawn the player inside of a wall in the constructor
-        const collisions = this.collider.getCollision();
-        let target = this.position.asVector();
+        const readjustment = this.collider.resolveCollisions(
+            this.position.asVector().subtract(origin),
+            1
+        );
+        this.position.add(readjustment);
 
-        while (true) {
-            const { value: collision, done } = collisions.next();
-
-            if (done) {
-                this.groundOverride -= 1;
-                break;
-            }
-
-            const { xStart, xEnd, yStart, yEnd } = collision.getBounds();
-            const difference = target.subtract(origin);
-
-            // TEMP (hacky solution but when player hugs wall by going left and switches directions, they tp across wall. This prevents that since switching direction slows you down.)
-            if (difference.getMagnitude() >= 0.0) {
-                let nearX = (xStart - this.collider.w / 2 - origin.x) / difference.x;
-                let farX = (xEnd + this.collider.w / 2 - origin.x) / difference.x;
-                let nearY = (yStart - this.collider.h / 2 - origin.y) / difference.y;
-                let farY = (yEnd + this.collider.h / 2 - origin.y) / difference.y;
-
-                if (nearX > farX) {
-                    [farX, nearX] = [nearX, farX];
-                }
-                if (nearY > farY) {
-                    [farY, nearY] = [nearY, farY];
-                }
-
-                const horizontalHit = nearX > nearY;
-                const hitNear = horizontalHit ? nearX : nearY;
-
-                let normal = undefined;
-                if (horizontalHit) {
-                    if (difference.x >= 0) {
-                        normal = new Vector(-1, 0);
-                    } else {
-                        normal = new Vector(1, 0);
-                    }
-                } else {
-                    if (difference.y >= 0) {
-                        normal = new Vector(0, -1);
-                    } else {
-                        normal = new Vector(0, 1);
-                    }
-                }
-
-                if (hitNear && isFinite(hitNear)) {
-                    if (collision.id === 1) {
-                        const { x, y } = origin.add(difference.multiply(hitNear));
-
-                        if (horizontalHit) {
-                            this.velocity.x = 0;
-                            this.position.set(x, this.position.y);
-                        } else {
-                            // guarantee some frames of "grounded" where the first is this one and the second causes player to fall into hitbox (triggers collision)
-                            this.groundOverride = 4;
-                            this.velocity.y = 0;
-                            this.position.set(this.position.x, y);
-                        }
-
-                        // force player to not touch a wall anymore after collision resolution
-                        // this might get in the way of some potential features
-                        this.position.add(normal.multiply(0.01));
-                    }
-                }
-            }
+        // horizontal collision
+        if (readjustment.x !== 0) {
+            this.velocity.x = 0;
+        }
+        // vertical collision
+        if (readjustment.y !== 0) {
+            // guarantee some frames of "grounded" where the first is this one and the second causes player to fall into hitbox (triggers collision)
+            this.groundOverride = 2;
+            this.velocity.y = 0;
+        }
+        // no collision
+        if (readjustment.y === 0 && readjustment.x === 0) {
+            this.groundOverride -= 1;
         }
     }
 

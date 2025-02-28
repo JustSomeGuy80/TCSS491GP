f8d45cb577582e215bd198e6d860c49fa5e0e1f6
Hai Duong <149478749+HaixD@users.noreply.github.com>
2025-02-27 18:52:02 -0800

    Fix friction and set default font to Arial

--- src/player.js
+++ src/player.js
@@ -324,7 +324,7 @@ class Player {
         if (topReadjustment.y !== 0 || stairAdjustment.y !== 0) {
             // guarantee some frames of "grounded" where the first is this one and the second causes player to fall into hitbox (triggers collision)
             this.animationGroundFrames = 8;
-            this.physicsGroundFrames = 1;
+            this.physicsGroundFrames = 2;
             this.velocity.y = 0;
         }
         // no collision
